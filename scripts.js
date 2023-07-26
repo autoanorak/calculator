ba6076a5c983adc1b8061326d3524139f3b5ca8c
David Yu <32144987+autoanorak@users.noreply.github.com>
2023-07-25 22:43:40 -0700

    Add and refine functionality, not yet up to spec

--- scripts.js
+++ scripts.js
@@ -13,15 +13,21 @@ operators.forEach((button) => {
 })
 
 let runningDisplay = [];
-let firstOperand = null;
-let secondOperand = null;
-let result = null;
+let resetDisplay = true;
+let operand;
+let operator;
+let result;
 
 function manipulateDisplay(event) {  
   let buttonContent = event.target.textContent;
   let contentForDisplay = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.',
   ];
+
+  if (resetDisplay && runningDisplay[0]) {
+    display.value = '';
+    resetDisplay = false;
+  }
 
   if (event.target.getAttribute('id') === 'decimal') {
     if (display.value.split('').includes('.')) {
@@ -37,31 +43,65 @@ function manipulateDisplay(event) {
 
 
 function operate(event) {
+  let operator;
   let currentOperator = event.target.getAttribute('id');
-  
-  if (firstOperand === null) {
-    firstOperand = parseFloat(display.value);
-    runningDisplay.push(firstOperand, currentOperator);
+
+  if (!runningDisplay[0]) {
+    operand = parseFloat(display.value);
+    runningDisplay.push(operand, currentOperator);
+    console.log(`if (!runningDisplay[0] -- ${runningDisplay}`);
     return;
   } else {
-    secondOperand = parseFloat(display.value);
-    runningDisplay.push(secondOperand, currentOperator);
-    console.log('else reached');
+    operand = parseFloat(display.value);
+    operator = runningDisplay[1];
+    runningDisplay.push(operand);
+    resetDisplay = true;
+    console.log(`if (!runningDisplay[0] ... else -- ${runningDisplay}`);
   }
 
-  console.log(runningDisplay);
 
-  let operator = runningDisplay[1];
-
-  /* switch (operator) {
+  switch (operator) {
     case 'add':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      return a / b;
-  } */
+      result = runningDisplay[0] + runningDisplay[2];
+      display.value = result;
+      resetDisplay = true;
+      runningDisplay = [];
+      runningDisplay.push(result, currentOperator);
+      console.log(`switch (operator) case 'add' -- ${runningDisplay}`);
+      break;
+    case 'subtract':
+      result = runningDisplay[0] - runningDisplay[2];
+      display.value = result;
+      resetDisplay = true;
+      runningDisplay = [];
+      runningDisplay.push(result, currentOperator);
+      console.log(`switch (operator) case 'subtract' -- ${runningDisplay}`);
+      break;
+    case 'multiply':
+      result = runningDisplay[0] * runningDisplay[2];
+      display.value = result;
+      resetDisplay = true;
+      runningDisplay = [];
+      runningDisplay.push(result, currentOperator);
+      console.log(`switch (operator) case 'multiply' -- ${runningDisplay}`);
+
+      break;
+    case 'divide':
+      result = runningDisplay[0] / runningDisplay[2];
+      display.value = result;
+      resetDisplay = true;
+      runningDisplay = [];
+      runningDisplay.push(result, currentOperator);
+      console.log(`switch (operator) case 'divide' -- ${runningDisplay}`);
+      break;
+    case 'equals':
+      result = runningDisplay[0];
+      display.value = result;
+      resetDisplay = true;
+      runningDisplay = [];
+      runningDisplay.push(result, currentOperator);
+      console.log(`switch (operator) case 'equals' -- ${runningDisplay}`);
+      break;
+  }
 }
 
