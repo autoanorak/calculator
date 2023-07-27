e33fc2f622018d2641f1ab0ac32a3dd19cb29bdf
David Yu <32144987+autoanorak@users.noreply.github.com>
2023-07-27 12:05:05 -0700

    Add keyboard support, tweak functions; further features and testing needed

--- scripts.js
+++ scripts.js
@@ -12,6 +12,8 @@ operators.forEach((button) => {
   button.addEventListener('mouseup', (event) => event.target.classList.remove('pressed'));
 })
 
+display.addEventListener('keydown', keyboardActions);
+
 let firstOperand;
 let currentOperator;
 let secondOperand;
@@ -19,18 +21,41 @@ let nextOperator;
 let result;
 
 let resetDisplay = false;
+let displayUpdated = false;
+
+function keyboardActions(event) {
+  let isDigitOrDecimal = /^[.\d]$/.test(event.key);
+
+  if (event.key === 'Backspace') {
+    return;
+  } else {
+    event.preventDefault();
+  }
+
+  if (isDigitOrDecimal) {
+    manipulateDisplay(event);
+  }
+
+  let operators = ['+', '-', '*', ];
+
+}
 
 function manipulateDisplay(event) {  
   let buttonContent = event.target.textContent;
-  let contentForDisplay = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.',
-  ];
+  let contentForDisplay = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.' ];
+
+  if (event.target.id === 'display') {
+    buttonContent = event.key;
+  } else {
+    buttonContent = event.target.textContent;
+  }
 
   // When a digit or decimal is clicked, reset the display if the first operand
   // and the current operator have been entered
   if (resetDisplay) {
     display.value = '';
     resetDisplay = false;
+    displayUpdated = false;
   }
 
   // Clear display and empty variables when 'clear' button is pressed
@@ -43,7 +68,7 @@ function manipulateDisplay(event) {
   }
   
   // Specify behaviour for when decimal button is clicked
-  if (event.target.getAttribute('id') === 'decimal') {
+  if (buttonContent === '.') {
     if (display.value.split('').includes('.')) {
       return;
     }
@@ -51,40 +76,47 @@ function manipulateDisplay(event) {
   }
 
   // Populate display with numbers
-  if (contentForDisplay.includes(Number(buttonContent))) {
+  if (contentForDisplay.includes(parseInt(buttonContent))) {
+    if (currentOperator) {
+      displayUpdated = true;
+    }
+
     display.value += buttonContent;
   }
 }
 
 function operate(event) {
-  
+  let operator = parseOperator(event);
+
   checkInputs: 
-  // If there is no first operand (i.e. user has not yet input any numbers, or
-  // there is no result from a previous calculation)
   if (!firstOperand) {
     firstOperand = parseFloat(display.value);
-    currentOperator = event.target.getAttribute('id');
+    currentOperator = operator;
     resetDisplay = true;
-    // Special behaviour for when the 'equals' button is clicked
-  } else if (currentOperator === 'equals') {
-    nextOperator = event.target.getAttribute('id');
-    if (nextOperator !== 'equals') {
-      currentOperator = nextOperator;
-      nextOperator = null;
+  } else if (operator === 'equals') {
+    nextOperator = operator;
+
+    if (displayUpdated) {
+      secondOperand = parseFloat(display.value);
+      displayUpdated = true;
+      resetDisplay = true;
     }
     break checkInputs;
-  // General behaviour for when there is already a first
-  } else {
-    secondOperand = parseFloat(display.value);
-    nextOperator = event.target.getAttribute('id');
-    resetDisplay = true;
+  } else if (firstOperand && !secondOperand) {
+    console.log('firstOperand set, no secondOperand');
+    nextOperator = operator;
+    
+    if (displayUpdated) {
+      secondOperand = parseFloat(display.value);
+      displayUpdated = true;
+      resetDisplay = true;
+    }
   }
 
   // Conduct operations only if inputs exist
   if (firstOperand && currentOperator && secondOperand) {
     switch (currentOperator) {
       case 'add':
-        console.log('switch add reached');
         result = firstOperand + secondOperand;
         display.value = result;
         firstOperand = result;
@@ -92,7 +124,6 @@ function operate(event) {
         currentOperator = nextOperator;
         break;
       case 'subtract':
-        console.log('switch subtract reached');
         result = firstOperand - secondOperand;
         display.value = result;
         firstOperand = result;
@@ -100,7 +131,6 @@ function operate(event) {
         currentOperator = nextOperator;
         break;
       case 'multiply':
-        console.log('switch multiply reached');
         result = firstOperand * secondOperand;
         display.value = result;
         firstOperand = result;
@@ -108,8 +138,14 @@ function operate(event) {
         currentOperator = nextOperator;
         break;
       case 'divide':
-        console.log('switch divide reached');
         result = firstOperand / secondOperand;
+        display.value = result;
+        firstOperand = result;
+        secondOperand = null;
+        currentOperator = nextOperator;
+        break;
+      case 'modulo':
+        result = firstOperand % secondOperand;
         display.value = result;
         firstOperand = result;
         secondOperand = null;
@@ -120,3 +156,24 @@ function operate(event) {
   
 }
 
+
+function parseOperator(event) {
+  if (event.target.id === 'display') {
+    switch(event.key) {
+      case '+':
+        return 'add';
+      case '-':
+        return 'subtract';
+      case '*':
+        return 'muiltiply';
+      case '/':
+        return 'divide';
+      case '%':
+        return 'modulo';
+      case '=':
+        return 'equals';
+    }
+  } else {
+    return event.target.getAttribute('id');
+  }
+}
